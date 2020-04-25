bd84789f401f620af9f0dfbc3a554b5596d24870
anandrkskd <anandrkskd@gmail.com>
2020-04-26 02:33:06 +0530

    try 4

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t anandrkskd/flaskapp:latest -t anandrkskd/flaskapp:$SHA -f .
+docker build -t anandrkskd/flaskapp:$SHA -t anandrkskd/flaskapp:latest .
+docker build -t anandrkskd/flaskapp:latest .
 
 docker push anandrkskd/flaskapp:latest
 docker push anandrkskd/flaskapp:$SHA
 
-kubectl apply -f ./flaskblog/k8s
+kubectl apply -f ./k8s
 kubectl set image deployments/flask-deployment flask=anandrkskd/flaskapp:$SHA
