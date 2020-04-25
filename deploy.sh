a67a978ae418851ecc600fed082e64bcc2bfc799
anandrkskd <anandrkskd@gmail.com>
2020-04-26 03:24:44 +0530

    container name changed

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t anandrkskd/flaskapp:latest -tanandrkskd/flaskapp:$SHA -f .
 docker build -t anandrkskd/flaskapp:$SHA -t anandrkskd/flaskapp:latest .
 docker build -t anandrkskd/flaskapp:latest .
 
@@ -6,4 +7,4 @@ docker push anandrkskd/flaskapp:latest
 docker push anandrkskd/flaskapp:$SHA
 
 kubectl apply -f ./k8s
-kubectl set image deployments/flask-deployment flask=anandrkskd/flaskapp:$SHA
+kubectl set image deployments/flask-deployment falsk=anandrkskd/flaskapp:$SHA
